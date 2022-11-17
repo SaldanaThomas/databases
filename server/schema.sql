5f6d2e1cdb2c8b8e59c93cf132a6d1b9b981e0cb
Thomas Saldana <saldana.tom@gmail.com>
2022-11-16 17:00:27 -0800

    Step 1 Complete

--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,31 @@
+DROP DATABASE IF EXISTS chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE rooms (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  roomname VARCHAR(20)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE messages (
+  message_id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(24),
+  text VARCHAR(20),
+  campus VARCHAR(20),
+  created_at VARCHAR(24),
+  github_handle VARCHAR(24),
+  updated_at VARCHAR(20),
+  room INT,
+  FOREIGN KEY (room) REFERENCES rooms(id)
+);
 
-
-
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(20)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
-
